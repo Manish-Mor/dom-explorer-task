aa9f135cced6eca6bc3094347f4e3e1d9013bfcc
Manish Mor <sachinmor122@gmail.com>
2026-08-12 23:11:05 +0530

    update delete btn

--- script.js
+++ script.js
@@ -5,36 +5,52 @@ const taskList = document.querySelector("#taskList");
 // const darkLightModeButton = document.querySelector("#Dark&light-mode");
 const form = document.querySelector("form");
 const taskLists = document.querySelector(".task-lists");
-const deleteBtn = document.querySelector(".deletebtn")
+
 
 const array = [
     {
-       
+       taskTitle1: "Homework",
+       category1: "Study"
+    },
+    {
+       taskTitle1: "Gym",
+       category1: "work"
+    },
+    {
+       taskTitle1: "create project",
+       category1: "study"
     }
 ];
 
 
 const ui = ()=>{
-       
+    taskLists.innerHTML = "";
+       array.forEach((elm, index) => {
         taskLists.innerHTML += `
         <div class="task-card">
            
 
-            <div class="task-item-title"> <h2>Title :- ${taskTitle.value }</h2></div>
-            <div class="task-item-category"><h2>Category :- ${category.value }</h2></div>
+            <div class="task-item-title"> <h2>Title :- ${elm.taskTitle1 }</h2></div>
+            <div class="task-item-category"><h2>Category :- ${elm.category1 }</h2></div>
             <div class="task-item-status"><h2>Status :- </h2></div>
-            <div class="task-item-actions"><button>Edit</button> <button class="deletebtn">Delete</button></div>
+            <div class="task-item-actions"><button>Edit</button> <button onclick="deleteBtn(${index})" class="deletebtn" >Delete</button></div>
         </div>`
+       });
 }
 
 
+   
 
-
-
+ui()
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    array.push({ task: taskTitle.value, category: category.value });
+    let taskTitle1 = taskTitle.value;
+    let category1 = category.value;
+    array.push({ 
+        taskTitle1,
+        category1,
+     });
   
 
     ui()
@@ -42,3 +58,8 @@ form.addEventListener("submit", (e) => {
 form.reset();
 })
 
+
+let deleteBtn = (index)=>{
+    array.splice(index, 1);
+    ui();
+}
