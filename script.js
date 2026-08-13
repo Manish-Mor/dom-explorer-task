312abcbd06b933643de542784cd5ef8131dacd0d
Manish Mor <sachinmor122@gmail.com>
2026-08-13 22:24:53 +0530

    update complete button & trying edit button work

--- script.js
+++ script.js
@@ -5,20 +5,23 @@ const taskList = document.querySelector("#taskList");
 // const darkLightModeButton = document.querySelector("#Dark&light-mode");
 const form = document.querySelector("form");
 const taskLists = document.querySelector(".task-lists");
-
+const editButtonSection = document.querySelector(".editbutton-section");
 
 const array = [
     {
        taskTitle1: "Homework",
-       category1: "Study"
+       category1: "Study",
+       status1: "Pending"
     },
     {
        taskTitle1: "Gym",
-       category1: "work"
+       category1: "work",
+       status1: "Complete"
     },
     {
        taskTitle1: "create project",
-       category1: "study"
+       category1: "study",
+       status1: "Pending"
     }
 ];
 
@@ -32,8 +35,8 @@ const ui = ()=>{
 
             <div class="task-item-title"> <h2>Title :- ${elm.taskTitle1 }</h2></div>
             <div class="task-item-category"><h2>Category :- ${elm.category1 }</h2></div>
-            <div class="task-item-status"><h2>Status :- </h2></div>
-            <div class="task-item-actions"><button>Edit</button> <button onclick="deleteBtn(${index})" class="deletebtn" >Delete</button></div>
+            <div class="task-item-status"><h2>Status :- ${elm.status1} </h2></div>
+            <div class="task-item-actions"><button  onclick="editBtn(${index})" >Edit</button> <button onclick="taskComplete(${index})"> Complete</button><button onclick="deleteBtn(${index})" class="deletebtn" >Delete</button></div>
         </div>`
        });
 }
@@ -47,9 +50,11 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     let taskTitle1 = taskTitle.value;
     let category1 = category.value;
+    let status1 = "Pending"
     array.push({ 
         taskTitle1,
         category1,
+        status1,
      });
   
 
@@ -62,4 +67,23 @@ form.reset();
 let deleteBtn = (index)=>{
     array.splice(index, 1);
     ui();
+}
+
+let editBtn = (index)=>{
+    // editButtonSection.style.display ="block"
+    // editButtonSection.innerHTML =`<div class="editbuttion">
+    //                 <div class="task-item-title"> <h2>Title :- ${elm.taskTitle1 }</h2></div>
+    //                 <div class="task-item-category"><h2>Category :- ${elm.category1 }</h2></div>
+    //                 <div class="task-item-status"><h2>Status :- ${elm.status1} </h2></div>
+    //                 <div><button>Submit</button></div>
+    //             </div>`
+    // make the form which activate when the click on the button and when click on the submit the ui update then , show and hide when click on the edit btn and submit btn 
+    array[index].taskTitle1= taskTitle.value;
+    array[index].category1 = category.value;
+    
+}
+
+let taskComplete = (index)=>{
+ array[index].status1= "Complete";
+ ui();
 }
