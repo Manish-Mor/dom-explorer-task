e83dae00b2b2951c65578bcd7e4481cc3204f7a1
Manish Mor <sachinmor122@gmail.com>
2026-08-15 22:38:28 +0530

    edit button and submit button is complete

--- script.js
+++ script.js
@@ -8,84 +8,104 @@ const taskLists = document.querySelector(".task-lists");
 const editButtonSection = document.querySelector(".editbutton-section");
 
 const array = [
-    {
-       taskTitle1: "Homework",
-       category1: "Study",
-       status1: "Pending"
-    },
-    {
-       taskTitle1: "Gym",
-       category1: "work",
-       status1: "Complete"
-    },
-    {
-       taskTitle1: "create project",
-       category1: "study",
-       status1: "Pending"
-    }
+  {
+    taskTitle1: "Homework",
+    category1: "Study",
+    status1: "Pending",
+  },
+  {
+    taskTitle1: "Gym",
+    category1: "work",
+    status1: "Complete",
+  },
+  {
+    taskTitle1: "create project",
+    category1: "study",
+    status1: "Pending",
+  },
 ];
 
-
-const ui = ()=>{
-    taskLists.innerHTML = "";
-       array.forEach((elm, index) => {
-        taskLists.innerHTML += `
+const ui = () => {
+  taskLists.innerHTML = "";
+  array.forEach((elm, index) => {
+    taskLists.innerHTML += `
         <div class="task-card">
            
 
-            <div class="task-item-title"> <h2>Title :- ${elm.taskTitle1 }</h2></div>
-            <div class="task-item-category"><h2>Category :- ${elm.category1 }</h2></div>
+            <div class="task-item-title"> <h2>Title :- ${elm.taskTitle1}</h2></div>
+            <div class="task-item-category"><h2>Category :- ${elm.category1}</h2></div>
             <div class="task-item-status"><h2>Status :- ${elm.status1} </h2></div>
             <div class="task-item-actions"><button  onclick="editBtn(${index})" >Edit</button> <button onclick="taskComplete(${index})"> Complete</button><button onclick="deleteBtn(${index})" class="deletebtn" >Delete</button></div>
-        </div>`
-       });
-}
+        </div>`;
+  });
+};
 
-
-   
-
-ui()
+ui();
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let taskTitle1 = taskTitle.value;
-    let category1 = category.value;
-    let status1 = "Pending"
-    array.push({ 
-        taskTitle1,
-        category1,
-        status1,
-     });
-  
+  e.preventDefault();
+  let taskTitle1 = taskTitle.value;
+  let category1 = category.value;
+  let status1 = "Pending";
+  array.push({
+    taskTitle1,
+    category1,
+    status1,
+  });
 
+  ui();
+  console.log("Task added");
+  form.reset();
+});
+
+let deleteBtn = (index) => {
+  array.splice(index, 1);
+  ui();
+};
+
+let editBtn = (index) => {
+  let edit = (editButtonSection.style.display = "flex");
+     let taskname =array[index]
+  editButtonSection.innerHTML = `<div class="editbuttion">
+                   <input class="editbutton-title"  required placeholder="Title" value="${taskname.taskTitle1}">
+                    
+                       <select required  class="edit-category" ">
+                           <option value="${taskname.category1}">Select category</option>
+                          <option value="Work">Work</option>
+                          <option value="Personal">Personal</option>
+                          <option value="Study">Study</option>
+                          <option value="Others">Others</option>
+                       </select>
+                
+                   
+                    <div class="editbtn-button">   <button id="editbtn-cancel" onclick="cancelBtn(${index})">Cancel</button><button id="submitbtn-submit" onclick="submitBtn(${index})">Submit</button></div>
+                </div>`;
+  // make the form which activate when the click on the button and when click on the submit the ui update then , show and hide when click on the edit btn and submit btn
+  // array[index].taskTitle1= taskTitle.value;
+  // array[index].category1 = category.value;
+  console.log(edit);
+  ui();
+};
+
+const editBtnSubmit = document.querySelector("#submitbtn-submit");
+ let submitBtn = (index)=>{
+    const editTitle = document.querySelector(".editbutton-title");
+const editCategory = document.querySelector(".edit-category");
+  if(editTitle ==="" || editCategory ==="") return;
+    array[index].taskTitle1 = editTitle.value;
+    array[index].category1 = editCategory.value;
+    editButtonSection.style.display = "none";
+  editButtonSection.innerHTML = "";
     ui()
-    console.log("Task added");
-form.reset();
-})
-
-
-let deleteBtn = (index)=>{
-    array.splice(index, 1);
+ }
+const editBtnCancel = document.querySelector("#editbtn-cancel")
+let cancelBtn = (index)=>{
+    editButtonSection.style.display = "none";
+  editButtonSection.innerHTML = "";
     ui();
 }
 
-let editBtn = (index)=>{
-   let edit = editButtonSection.style.display ="flex";
-//    editButtonSection.style.
-    editButtonSection.innerHTML =`<div class="editbuttion">
-                    <div class="task-item-title"> <h2>Title :- </h2></div>
-                    <div class="task-item-category"><h2>Category :- </h2></div>
-                    <div class="task-item-status"><h2>Status :- </h2></div>
-                    <div><button>Submit</button></div>
-                </div>`
-    // make the form which activate when the click on the button and when click on the submit the ui update then , show and hide when click on the edit btn and submit btn 
-    // array[index].taskTitle1= taskTitle.value;
-    // array[index].category1 = category.value;
-    console.log(edit)
-    ui()
-}
-
-let taskComplete = (index)=>{
- array[index].status1= "Complete";
- ui();
-}
+let taskComplete = (index) => {
+  array[index].status1 = "Complete";
+  ui();
+};
